8a99296519c8290b741694f82bbc3207b7a4ef56
Ishan-1999 <1999ishan.sharma@gmail.com>
2020-09-06 14:02:36 +0530

    index page content

--- ReviewApp/admin.py
+++ ReviewApp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Review, Contact, Menu
 
 
+admin.site.site_header = 'KokaniKitchen Administration'
+
 # Register your models here.
 admin.site.register(Review)
 admin.site.register(Contact)
